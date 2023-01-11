a2e8fde4a4da051301b1efc4ea17c2936f7bb076
Neil Parrilla <neil.parrilla@gmail.com>
2023-01-10 18:17:37 -0800

    Complete basic custom hook simple implementation

---
+++ src/components/BackwardCounter.js
@@ -0,0 +1,11 @@
+import Card from './Card';
+import useCounter from '../hooks/UseCounter';
+
+const BackwardCounter = () => {
+
+  const counter = useCounter(false);
+
+  return <Card>{counter}</Card>;
+};
+
+export default BackwardCounter;
